30ba2b3cba20e47fa1658448dff7f01f944cfa43
sam002696 <sadmansakib221@gmail.com>
2021-10-27 12:04:19 +0600

    small changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
@@ -15,7 +16,7 @@ app.get('/', (req, res) => {
     res.send('hello my first node fsfasfqfqe');
 })
 
-const uri = "mongodb+srv://sam002696:40EaVOpY036j4xRf@cluster0.yftux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yftux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
