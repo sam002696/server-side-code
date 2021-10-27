0485316d21eb651376610156b05b0945d8b25402
sam002696 <sadmansakib221@gmail.com>
2021-10-27 12:52:59 +0600

    added port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //middleware
 
